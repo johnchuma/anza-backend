93ed6be1363e41986f6b3feef8b1cefa02c0f142
johnchuma <johnvchuma@gmail.com>
2023-11-20 17:25:00 +0300

    Modifying review routes

--- modules/review/review.routes.js
+++ modules/review/review.routes.js
@@ -3,9 +3,9 @@ const { validateJWT } = require("../../utils/validateJWT")
 const { createReview, updateReview, getReviews, getReview } = require('./review.controller')
 
 const router = Router()
-router.post("/",validateJWT,createReview)
-router.patch("/:uuid",validateJWT,updateReview)
-router.get("/:uuid",validateJWT,getReview)
-router.get("/",validateJWT,getReviews)
+router.post("/",createReview)
+router.patch("/:uuid",updateReview)
+router.get("/:uuid",getReview)
+router.get("/",getReviews)
 
 module.exports = router
