df3be87178960b18faea68d30b6f4446ba36a590
gitderekz <dereklyatuu@gmail.com>
2023-11-20 12:40:43 +0300

    payment

--- modules/product/product.routes.js
+++ modules/product/product.routes.js
@@ -5,14 +5,14 @@ const { createProduct, updateProduct, getProduct, getProducts, getFeaturedProduc
 
 const router = Router()
 router.post("/:uuid",validateJWT,createProduct)
-router.get("/featured",validateJWT,getFeaturedProducts)
-router.get("/top_selling",validateJWT,getTopSellingProducts)
-router.get("/top_rated",validateJWT,getTopRatedProducts)
-router.get("/:uuid",validateJWT,getProduct)
+router.get("/featured",getFeaturedProducts)
+router.get("/top_selling",getTopSellingProducts)
+router.get("/top_rated",getTopRatedProducts)
+router.get("/:uuid",getProduct)
 router.patch("/:uuid",validateJWT,updateProduct)
-router.get("/business/:uuid",validateJWT,BusinessProducts)
+router.get("/business/:uuid",BusinessProducts)
 router.get("/",getProducts)
-router.get('/business_sector/:uuid',validateJWT,getBusinessSectorProducts)
+router.get('/business_sector/:uuid',getBusinessSectorProducts)
 router.delete('/:uuid',validateJWT,deleteProduct)
 
 router.get("/search/:itemName",searchProduct) //PASS PRODUCT NAME ON URL
