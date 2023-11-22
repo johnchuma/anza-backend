6e5eff3c0329e7180b0672451560a0a0d25d1481
gitderekz <dereklyatuu@gmail.com>
2023-11-22 13:47:04 +0300

    debug order

--- modules/order/order.routes.js
+++ modules/order/order.routes.js
@@ -3,7 +3,7 @@ const { validateJWT } = require("../../utils/validateJWT")
 const { createOrder, deleteOrderProduct, deleteOrder, getSpecificBusinessOrders, getCustomerOrders,updateOrderProduct } = require('./order.controller')
 
 const router = Router()
-router.post("/:uuid",validateJWT,createOrder)
+router.post("/",validateJWT,createOrder)
 router.get("/business/:uuid",validateJWT,getSpecificBusinessOrders)
 router.get("/",validateJWT,getCustomerOrders)
 router.delete('/orderProduct/:uuid',validateJWT,deleteOrderProduct)
