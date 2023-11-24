405ba887a099a18cae92b52babe9555a57bfc4a6
gitderekz <dereklyatuu@gmail.com>
2023-11-24 11:13:30 +0300

    seller counts

--- modules/seller/seller.controller.js
+++ modules/seller/seller.controller.js
@@ -7,130 +7,136 @@ const {Op} = require("sequelize");
   const getCounts = async(req,res)=>{
     var totalBuyingPrice = 0,totalSellingPrice = 0, profit = 0
     const user = req.user
-    try {
-        const pending = await OrderProduct.count({
-            include:{
-                model:Product,
+
+    if (user.Business) {
+        try {
+            const pending = await OrderProduct.count({
+                include:{
+                    model:Product,
+                    include:{
+                        model:Business,
+                        where:{
+                            userId:user.id
+                        }
+                    }
+                },
+            where:{
+                status: "waiting"
+            }
+            })
+            const complete = await OrderProduct.count({
+                include:{
+                    model:Product,
+                    include:{
+                        model:Business,
+                        where:{
+                            userId:user.id
+                        }
+                    }
+                },
+            where:{
+                status: "delivered"
+            }
+            })
+            // CALCULATE PROFIT
+            const business = await Business.findOne({
+                where:{
+                    userId:user.id
+                }
+            })
+            const profit_query = await OrderProduct.findAll({
+                include:{
+                    model:Product,
+                    where:{
+                        businessId:business.id,
+                    }
+                },
+                // attributes:{
+                //     include: [
+                //         [
+                //             Sequelize.literal(`(
+                //                 SELECT buyingPrice
+                //                 FROM Products AS product
+                //                 WHERE
+                //                     id = OrderProduct.productId
+                //             )`),
+                //             'buyingPrice'
+                //         ],
+                //         [
+                //             Sequelize.literal(`(
+                //                 SELECT sellingPrice
+                //                 FROM Products AS product
+                //                 WHERE
+                //                     id = OrderProduct.productId
+                //             )`),
+                //             'sellingPrice'
+                //         ]
+                //     ],
+                // },
+            })
+            for (let index = 0; index < profit_query.length; index++) {
+                const element = profit_query[index];
+                // res.send({data: element})
+                // res.send({data: element.quantity})
+                // res.send({data: element.Product.buyingPrice})
+
+                totalBuyingPrice += element.Product.buyingPrice * element.quantity
+                totalSellingPrice += element.Product.sellingPrice * element.quantity
+            }
+            profit = totalSellingPrice - totalBuyingPrice
+            // END CALCULATE PROFIT
+            const sales = await OrderProduct.count({
+                include:{
+                    model:Product,
+                    include:{
+                        model:Business,
+                        where:{
+                            userId:user.id
+                        }
+                    }
+                },
+            })
+
+            const products = await Product.count({
                 include:{
                     model:Business,
                     where:{
-                        userId:user.id
+                        userId:user.id,
                     }
                 }
-            },
-          where:{
-            status: "waiting"
-          }
-        })
-        const complete = await OrderProduct.count({
-            include:{
-                model:Product,
+            })
+            const in_stock = await Product.count({
                 include:{
                     model:Business,
                     where:{
-                        userId:user.id
+                        userId:user.id,
                     }
-                }
-            },
-          where:{
-            status: "delivered"
-          }
-        })
-        // CALCULATE PROFIT
-        const business = await Business.findOne({
-            where:{
-                userId:user.id
-            }
-        })
-        const profit_query = await OrderProduct.findAll({
-            include:{
-                model:Product,
+                },
                 where:{
-                    businessId:business.id,
+                    amount:{[Op.gt]:0},
                 }
-            },
-            // attributes:{
-            //     include: [
-            //         [
-            //             Sequelize.literal(`(
-            //                 SELECT buyingPrice
-            //                 FROM Products AS product
-            //                 WHERE
-            //                     id = OrderProduct.productId
-            //             )`),
-            //             'buyingPrice'
-            //         ],
-            //         [
-            //             Sequelize.literal(`(
-            //                 SELECT sellingPrice
-            //                 FROM Products AS product
-            //                 WHERE
-            //                     id = OrderProduct.productId
-            //             )`),
-            //             'sellingPrice'
-            //         ]
-            //     ],
-            // },
-        })
-        for (let index = 0; index < profit_query.length; index++) {
-            const element = profit_query[index];
-            // res.send({data: element})
-            // res.send({data: element.quantity})
-            // res.send({data: element.Product.buyingPrice})
-
-            totalBuyingPrice += element.Product.buyingPrice * element.quantity
-            totalSellingPrice += element.Product.sellingPrice * element.quantity
-        }
-        profit = totalSellingPrice - totalBuyingPrice
-        // END CALCULATE PROFIT
-        const sales = await OrderProduct.count({
-            include:{
-                model:Product,
+            })
+            const out_stock = await Product.count({
                 include:{
                     model:Business,
                     where:{
-                        userId:user.id
+                        userId:user.id,
                     }
+                },
+                where:{
+                    amount:{[Op.lte]:0},
                 }
-            },
-        })
+            })
 
-        const products = await Product.count({
-            include:{
-                model:Business,
-                where:{
-                    userId:user.id,
-                }
-            }
-        })
-        const in_stock = await Product.count({
-            include:{
-                model:Business,
-                where:{
-                    userId:user.id,
-                }
-            },
-            where:{
-                amount:{[Op.gt]:0},
-            }
-        })
-        const out_stock = await Product.count({
-            include:{
-                model:Business,
-                where:{
-                    userId:user.id,
-                }
-            },
-            where:{
-                amount:{[Op.lte]:0},
-            }
-        })
-
-        successResponse(res,{pending:pending, complete:complete, profit: profit, sales: sales, products:products, in_stock:in_stock, out_stock:out_stock,
-            totalBuyingPrice:totalBuyingPrice,totalSellingPrice:totalSellingPrice,  })
-    } catch (error) {
-        errorResponse(res,error)
+            successResponse(res,{pending:pending, complete:complete, profit: profit, sales: sales, products:products, in_stock:in_stock, out_stock:out_stock,
+                totalBuyingPrice:totalBuyingPrice,totalSellingPrice:totalSellingPrice,  })
+        } catch (error) {
+            errorResponse(res,error)
+        }        
+    } else {
+        res.status(403).json({message:"You don't own a business!"});
     }
+
   }
 
   module.exports = {
